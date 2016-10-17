a1da8aa776d92000107b9f63e1053b751794073f
webeasyplay <webeasyplay.cr@gmail.com>
2016-10-17 11:38:04 +0800

    Initial Commit

---
+++ Hello World/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by ShiLuChang on 2016/10/17.
+//  Copyright © 2016年 張仕陸. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
